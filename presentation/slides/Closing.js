d397b95364b02b80890bdebe6c1bbd7cb7af5352
Michael Leners <micleners@gmail.com>
2020-03-12 12:54:57 -0500

    change closing slide

--- presentation/slides/Closing.js
+++ presentation/slides/Closing.js
@@ -22,77 +22,73 @@ import {
   Text,
   Link
 } from "spectacle";
+import { Flex } from "rebass";
 
 const images = {
-  sai: require("../../assets/sai.png").default,
+  bug: require("../../assets/bug.svg").default,
+  sweep_bug: require("../../assets/sweep_bug.svg").default,
+  github: require("../../assets/Github.svg").default,
   twitter: require("../../assets/twitter.png").default
 };
 
 export const Closing = () => (
   <Slide transition={["zoom"]} bgColor="dark">
-    <H1Norm>Questions or Comments?</H1Norm>
-    <List style={{ marginLeft: "200" }} textColor="text">
-      <br />
-      <br />
-      <Heading size={5} lineHeight={1} textColor="text">
-        This Presentation: cypress-retro.micleners.com
-      </Heading>
-      <br />
-
-      <Heading size={5} lineHeight={1} textColor="text">
-        Article: github.com/micleners/cypress-retro
-      </Heading>
-      <br />
-
-      <Heading size={5} lineHeight={1} textColor="text">
-        Demo Repo: github.com/micleners/cypress-cotd
-      </Heading>
-    </List>
-
-    <br />
-    <br />
-    <br />
-
-    <FlexRowCenter>
-      <div>
-        <Heading size={4} caps lineHeight={1} textColor="text">
-          Michael Leners
+    <Flex alignItems="center" justifyContent="center">
+      <img src={images.bug} style={{ margin: 15 }} width="100px" />
+      <Flex flexDirection="column" alignItems="center" justifyContent="center">
+        <Heading size={4} lineHeight={1.1} textColor="lightText">
+          Cybernetic Entomology:
+          <br />
+          There's always one more bug
         </Heading>
-        <Heading size={6} lineHeight={1} textColor="darkText">
-          <div
-            style={{
-              display: "flex",
-              flexDirection: "row",
-              justifyContent: "flex-end"
-            }}
+        <br />
+        <Heading size={3}>
+          <span
+            style={{ color: themeColors.yellow, textTransform: "uppercase" }}
           >
-            <img
-              src={images.twitter}
-              style={{ marginRight: 15 }}
-              width="50px"
-            />{" "}
-            @micLeners
-          </div>
+            Questions or Comments?
+          </span>
         </Heading>
-      </div>
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "flex-start",
-          alignItems: "flex-start"
-        }}
-      >
-        <Heading
-          size={6}
-          lineHeight={0.3}
-          style={{ margin: "20px 0 0 0" }}
-          textColor="darkText"
-        >
-          Hosted by
+      </Flex>
+      <img src={images.sweep_bug} style={{ margin: 15 }} width="100px" />
+    </Flex>
+    <br />
+    <br />
+    <Text size={5} lineHeight={1} textColor="text">
+      <strong>This Presentation:</strong> cybernetic-entomology.micleners.com
+    </Text>
+    <br />
+
+    <Text size={5} lineHeight={1} textColor="text">
+      <strong>Repo:</strong> github.com/micleners/cybernetic-entomology
+    </Text>
+    <br />
+
+    <Text size={5} lineHeight={1} textColor="text">
+      <strong>Cypress Repo:</strong> github.com/micleners/cypress-retro
+    </Text>
+
+    <Flex
+      py={5}
+      flexDirection="column"
+      alignItems="center"
+      justifyContent="center"
+    >
+      <Heading size={4} caps lineHeight={1} textColor="text">
+        Michael Leners
+      </Heading>
+      <Flex alignItems="center" justifyContent="center">
+        <img
+          src={images.twitter}
+          style={{ margin: "0 15px" }}
+          width="70px"
+          height="auto"
+        />
+        <Heading size={6} lineHeight={1} textColor="text">
+          micLeners
         </Heading>
-        <img src={images.sai} width="500px" />
-      </div>
-    </FlexRowCenter>
+        <img src={images.github} style={{ margin: "0 15px" }} width="70px" />
+      </Flex>
+    </Flex>
   </Slide>
 );
